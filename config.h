1cfa5dd62d8473e84e4c53d1cf9624093bc50c18
Tiago Verçosa <vercosa@proton.me>
2022-10-29 17:25:20 -0300

    changed font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerd Font Mono:pixelsize=19:antialias=true:autohint=true";
+static char *font = "Iosevka SS08 Medium:pixelsize=20:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"JoyPixels:pixelsize=15:antialias=true:autohint=true",
